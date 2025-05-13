f6f55cca7728fb1f1837eee797a87f632b63a1d2
Rogerio-17 <rogeriojmf10@gmail.com>
2025-05-12 21:56:06 -0300

    feat: adiciona login por meio do google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,7 +3,7 @@ import NextAuth from "next-auth";
 import { firebaseCert } from "./firebase";
 import Google from "next-auth/providers/google";
 
-const { handlers, signIn, signOut, auth } = NextAuth({
+export const { handlers, signIn, signOut, auth } = NextAuth({
     adapter: FirestoreAdapter({
         credential: firebaseCert,
 
