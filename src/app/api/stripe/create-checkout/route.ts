5427c63b0a945c6a2684e6781cfa414063194473
Rogerio-17 <rogeriojmf10@gmail.com>
2025-05-25 13:33:05 -0300

    feat: adiciona webhook

--- src/app/api/stripe/create-checkout/route.ts
+++ src/app/api/stripe/create-checkout/route.ts
@@ -1,3 +1,5 @@
+import { auth } from "@/lib/auth";
+import { db } from "@/lib/firebase";
 import stripe from "@/lib/stripe";
 import { NextResponse } from "next/server";
 
@@ -6,8 +8,48 @@ export async function POST(request: Request) {
 
     const price = isSubscription ? process.env.STRIPE_SUBSCRIPTION_PRICE_ID : process.env.STRIPE_PRICE_ID;
 
+    const userSession = await auth()
+
+    if (!userSession || !userSession.user?.id || !userSession.user?.email) {
+        return NextResponse.json(
+            {
+                error: "User not authenticated",
+            },
+            { status: 401 }
+        )
+    }
+
+    const userId = userSession.user?.id;
+    const userEmail = userSession.user?.email;
+    const userName = userSession.user?.name;
+
+    const userRef = await db.collection("users").doc(userId)
+    const userDoc = await userRef.get();
+
+    let customerId
+
+    if (userDoc.exists) {
+        customerId = userDoc.data()?.customerId
+    }
+
+    if (!customerId) {
+        const newCustomer = await stripe.customers.create({
+            email: userEmail,
+            name: userName || "Sem Nome",
+            metadata: {
+                userId: userId,
+            },
+        })
+
+        const customerId = newCustomer.id
+
+        await userRef.set({
+            customerId,
+        })
+    }
+
     const session = await stripe.checkout.sessions.create({
-        //customer: ,
+        customer: customerId,
         line_items: [
             {
                 price: price,
@@ -18,6 +60,7 @@ export async function POST(request: Request) {
         payment_method_types: isSubscription ? ["card"] : ["card", "boleto"],
         success_url: `${request.headers.get("origin")}/${metadata.profileId}`,
         cancel_url: `${request.headers.get("origin")}/${metadata.profileId}/upgrade`,
+        client_reference_id: userId,
         metadata,
     })
 
